c8affd46d5fcc74787bdeef989c7349a41845b14
Austin Rude <rudedogg@users.noreply.github.com>
2021-11-26 11:36:05 -0700

    Better variable names and use smaller uint size

--- day1/main.zig
+++ day1/main.zig
@@ -5,11 +5,11 @@ const input = @embedFile("input.txt");
 pub fn main() !void {
     // https://github.com/ziglang/zig/blob/a2c546fea30de2caf1efb454d327e70270c07338/lib/std/mem.zig#L1732
     var outerIterator = std.mem.split(u8, input, "\n");
-    while (outerIterator.next()) |outerNumber| {
-        const outerUInt = try std.fmt.parseUnsigned(u32, outerNumber, 10);
+    while (outerIterator.next()) |outerLine| {
+        const outerUInt = try std.fmt.parseUnsigned(u16, outerLine, 10);
         var innerIterator = std.mem.split(u8, input, "\n");
-        while (innerIterator.next()) |innerNumber| {
-            const innerUInt = try std.fmt.parseUnsigned(u32, innerNumber, 10);
+        while (innerIterator.next()) |innerLine| {
+            const innerUInt = try std.fmt.parseUnsigned(u16, innerLine, 10);
             if (outerUInt + innerUInt == 2020) {
                 std.debug.print("Number 1: {d}\n", .{outerUInt});
                 std.debug.print("Number 2: {d}\n", .{innerUInt});
