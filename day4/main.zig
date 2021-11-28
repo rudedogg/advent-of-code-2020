38aa31c5edca305eac82f5386a1c58df3d8f0b9b
Austin Rude <rudedogg@users.noreply.github.com>
2021-11-28 11:30:33 -0700

    Add day 4 input and template main.zig

---
+++ day4/main.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+
+const input = @embedFile("input.txt");
+
+pub fn main() !void {
+    std.debug.print("Hello day {d}\n", .{4});
+    // try part1();
+    // try part2();
+}
+
+fn part1() !void {}
+
+fn part2() !void {}
