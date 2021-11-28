e2324804b2b4383fd8b4426877ade31cb6cd4481
Austin Rude <rudedogg@users.noreply.github.com>
2021-11-28 14:20:35 -0700

    No progress on solution, just saving

--- day4/part2.zig
+++ day4/part2.zig
@@ -3,7 +3,8 @@ const std = @import("std");
 const input = @embedFile("input.txt");
 
 const Passport = struct {
-    byr: bool = false,
+    // byr: bool = true,
+    byr: []u8 = "",
     iyr: bool = false,
     eyr: bool = false,
     hgt: bool = false,
@@ -13,17 +14,23 @@ const Passport = struct {
     cid: bool = false,
 
     fn isValid(self: *Passport) bool {
-        return self.byr and self.iyr and self.eyr and self.hgt and self.hcl and self.ecl and self.pid;
+        std.debug.print("Hi: {s}", .{self});
+        return true;
+        // return self.byr and self.iyr and self.eyr and self.hgt and self.hcl and self.ecl and self.pid;
     }
+    // fn byrValid() bool {
+    //     return byr.
+    // }
 };
 
 pub fn main() !void {
     std.debug.print("Hello day {d}, part 2\n", .{4});
-    // var req_fields = ["byr"]
     try part2();
 }
 
 fn part2() !void {
+    // const fieldNames = [_]u8{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"};
+
     var validPassportCount: u16 = 0;
 
     var passportIterator = std.mem.split(u8, input, "\n\n");
@@ -33,9 +40,14 @@ fn part2() !void {
         std.debug.print("Passport: \n", .{});
         var lineIterator = std.mem.split(u8, passport, "\n");
         while (lineIterator.next()) |line| {
+            // for (fields) |field| {
+            //     std.debug.print("{s}\n", .{field});
+            // }
+
             std.debug.print("{s}\n", .{line});
             // currentPassport.byr = currentPassport.byr or std.mem.indexOf(u8, line, "byr") != null;
-            currentPassport.byr = currentPassport.byr or std.mem.containsAtLeast(u8, line, 1, "byr");
+            // currentPassport.byr = currentPassport.byr or std.mem.containsAtLeast(u8, line, 1, "byr");
+            currentPassport.byr = "";
             currentPassport.iyr = currentPassport.iyr or std.mem.indexOf(u8, line, "iyr") != null;
             currentPassport.eyr = currentPassport.eyr or std.mem.indexOf(u8, line, "eyr") != null;
             currentPassport.hgt = currentPassport.hgt or std.mem.indexOf(u8, line, "hgt") != null;
